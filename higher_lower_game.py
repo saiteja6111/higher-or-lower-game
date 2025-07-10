c477ec7ee9c85a19c5b1f2887e7ad6f5091544f3
saiteja6111 <saitejain8055@gmail.com>
2025-07-10 20:02:12 +0530

    higher or lower game

---
+++ higher_lower_game.py
@@ -0,0 +1,50 @@
+import game_data
+import random
+
+
+
+def random1():
+    global random_value1
+    random_value1 = random.randint(0,len(game_data.data))
+
+def random2():
+    global random_value2
+    random_value2 = random.randint(0,len(game_data.data))
+
+random1()
+random2()
+start = True
+count = 0
+a = f"{game_data.data[random_value1]['name']}, a {game_data.data[random_value1]['description']}, from {game_data.data[random_value1]['country']}."
+while start:
+
+    #a = f"{game_data.data[random_value1]['name']}, a {game_data.data[random_value1]['description']}, from {game_data.data[random_value1]['country']}."
+
+    b = f"{game_data.data[random_value2]['name']}, a {game_data.data[random_value2]['description']}, from {game_data.data[random_value2]['country']}."
+
+    print(a+"\n")
+    print("VS\n")
+    print(b+"\n")
+    choice = input("Who has more followers? Type 'A' or 'B': ")
+    if choice.lower() == 'a':
+        if game_data.data[random_value1]['follower_count'] > game_data.data[random_value2]['follower_count'] or game_data.data[random_value1]['follower_count'] == game_data.data[random_value2]['follower_count'] :
+            a = b
+            random2()
+            count += 1
+            continue
+        elif game_data.data[random_value1]['follower_count'] < game_data.data[random_value2]['follower_count']:
+            print(f"Sorry, that's wrong. Final score: {count} ")
+            break
+    elif choice.lower() == 'b':
+        if game_data.data[random_value2]['follower_count'] > game_data.data[random_value1]['follower_count'] or game_data.data[random_value2]['follower_count'] == game_data.data[random_value1]['follower_count']:
+            a = b
+            random2()
+            count += 1
+            continue
+        elif game_data.data[random_value2]['follower_count'] < game_data.data[random_value1]['follower_count']:
+            print(f"Sorry, that's wrong. Final score: {count}")
+            break
+    
+    
+
+
